46fc3241c739f977587dc4b514870d137a08417c
tuanhung2342040 <55459655+tuanhung2342040@users.noreply.github.com>
2021-02-16 14:19:15 -0800

    Add file

---
+++ Problem2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem2;
+
+/**
+ *
+ * @author 2342040
+ */
+public class Problem2 {
+
+    public static void main(String[] args) {
+        int roomNum = 113;
+        double e = 2.71828;
+        String subject = "Computer Science";
+        
+        System.out.println("This is room # " + roomNum);
+        System.out.println("e is close to " + e);
+        System.out.println("I am learning a bit about " + subject);
+        
+        
+    }
+    
+}
